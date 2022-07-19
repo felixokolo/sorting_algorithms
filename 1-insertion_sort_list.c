47e31f5e90737a6a22b2e9ed54f8dbb24f5a8e27
felixokolo <pheelix.info@gmail.com>
2022-07-19 14:50:12 +0100

    additions

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * bubble_sort - function that sorts a doubly linked
+ * insertion_sort_list - function that sorts a doubly linked
  * list of integers in ascending order using the
  * Insertion sort algorithm
  * @list: List to sort
@@ -17,13 +17,14 @@ void insertion_sort_list(listint_t **list)
 	while (!sorted)
 	{
 		sorted = 1;
+		tmp = *list;
 		while(tmp)
 		{
 			if (tmp->next)
 			if (tmp->n > tmp->next->n)
 			{
 				sorted = 0;
-				swap(tmp, tmp->next);
+				swap(tmp, tmp->next, list);
 				print_list(*list);
 				tmp = tmp->prev;
 				while (tmp)
@@ -31,12 +32,13 @@ void insertion_sort_list(listint_t **list)
 					if (tmp->prev)
 					if (tmp->n < tmp->prev->n)
 					{
-						swap(tmp, tmp->prev);
+						swap(tmp->prev, tmp, list);
 						print_list(*list);
 					}
 					tmp = tmp->prev;
 				}
 			}
+			if(tmp)
 			tmp = tmp->next;
 		}
 	}
@@ -48,20 +50,23 @@ void insertion_sort_list(listint_t **list)
  * swap - function that swaps 2 nodes
  * @first: first nodes
  * @sec: second node
+ * @list: List head
  */
-void swap(listint_t *first, listint_t *sec)
+void swap(listint_t *first, listint_t *sec, listint_t **list)
 {
 	listint_t *tmp;
 
 	if (first->prev)
 	first->prev->next = sec;
+	else
+	*list = sec;
 	if (sec->next)
 	sec->next->prev = first;
 	tmp = first->next;
 	first->next = sec->next;
-	sec->next = tmp;
+	sec->next = first;
 	tmp = first->prev;
-	first->prev = sec->prev;
+	first->prev = sec;
 	sec->prev = tmp;
 
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array
+ * of integers in ascending order using the Selection sort algorithm
+ * @list: List to sort
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t c, j;
+	int tmp, tmp2;
+
+	for (c = 0; c < size; c++)
+	{
+		tmp = *(array + c);
+		tmp2 = -1;
+		for (j = c; j < size; j++)
+		{
+			if (*(array + j) < tmp)
+			{
+				tmp2 = j;
+				tmp = *(array + j);
+			}
+		}
+		if (tmp2 >= 0)
+		{
+			*(array + tmp2) = *(array + c);
+			*(array + c) = tmp;
+			print_array(array, size);
+		}
+	}
+}
