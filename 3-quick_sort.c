09d86f2f5231701e9cbdd984a198b907f8b249e8
felixokolo <pheelix.info@gmail.com>
2022-07-19 21:12:14 +0100

    correction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,22 +5,83 @@
  * @array: List to sort
  * @size: size of array
  */
+
 void quick_sort(int *array, size_t size)
 {
-	size_t i = 0, j = size - 1;
-	int dir = 0, sorted = 0, tmp;
+	if (!array || size < 2)
+		return;
+	quicker_sort(array, size, 0, size - 1);
+}
 
-	while (!sorted)
+/**
+ * quicker_sort - does the actual recursive QuickSort
+ * @array: array of integers
+ * @size: number of elements in array
+ * @lo: first index of array
+ * @hi: last index of array
+ *
+ * Return: nothing!
+ */
+void quicker_sort(int *array, size_t size, int lo, int hi)
+{
+	int idx;
+
+	if (lo < hi)
 	{
-		if (*(array + i) > *(array + j))
+		idx = partition(array, size, lo, hi);
+		quicker_sort(array, size, lo, idx - 1);
+		quicker_sort(array, size, idx + 1, hi);
+	}
+}
+/**
+ * partition - partition through an array of integers
+ * @array: array of integers
+ * @size: number of elements of array
+ * @lo: first index of array
+ * @hi: last index of array
+ *
+ * Description: all values must be accurate
+ * Return: new index position
+ */
+int partition(int *array, size_t size, int lo, int hi)
+{
+	int pivot = array[hi], idx = lo, xdi;
+
+	for (xdi = lo; xdi <= hi - 1; xdi++)
+	{
+		if (array[xdi] < pivot)
 		{
-			tmp = *(array + i);
-			*(array + i) = *(array + j);
-			*(array + j) = tmp;
-			dir = dir == 0 ? 1 : 0;
-			if (dir == 0)
-			j--;
-			else
-			i++;
+			if (idx != xdi)
+			{
+				swap(&array[idx], &array[xdi]);
+				print_array(array, size);
+			}
+			idx++;
 		}
+	}
+	if (pivot != array[idx])
+	{
+		swap(&array[idx], &array[hi]);
+		print_array(array, size);
+	}
+	return (idx);
+}
+
+/**
+ * swap - swaps the address values of two pointers
+ * @ptr_a: pointer to integer
+ * @ptr_b: pointer to integer
+ *
+ * Return: nothing!
+ */
+void swap(int *ptr_a, int *ptr_b)
+{
+	int tmp;
+
+	if (!ptr_a || !ptr_b)
+		return;
+
+	tmp = *ptr_a;
+	*ptr_a = *ptr_b;
+	*ptr_b = tmp;
 }
